62935b6ab07e315d837bc9190d46d531ea7d7337
sjlee <Administrator@WIN-1F5L2OG1CK9>
2014-12-13 21:30:36 +0900

    Creates int getMonthLastDay(), void SetID(int newID), int GetID(), void SetYear(int), int GetYear(), void SetMonth(int), int GetMonth(), void SetDay(int), int GetDay().
    NOT TESTED YET...

--- practiCal/src/com/tejava/practical/SingleEvent.java
+++ practiCal/src/com/tejava/practical/SingleEvent.java
@@ -11,18 +11,46 @@ public class SingleEvent
 
 	}
 
-	public void SetId(int id) throws Exception
+	private int getMonthLastDay()
+	{
+		switch (month) {
+		case 0:
+		case 2:
+		case 4:
+		case 6:
+		case 7:
+		case 9:
+		case 11:
+			return (31);
+
+		case 3:
+		case 5:
+		case 8:
+		case 10:
+			return (30);
+
+		default:
+			if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) {
+				return (29);
+			} else {
+				return (28);
+			}
+		}
+	}
+
+	public void SetId(int newID) throws Exception
 	{
 		try
 		{
-			if(id < 0)
+			if(newID < 0)
 			{
 				throw new Exception();
 			}
+			id = newID;
 		}
 		catch (Exception ex)
 		{
-			System.out.println("ID is " + id + ", ID should be positive!");
+			System.out.println("ID is " + newID + ", ID should be positive!");
 		}
 	}
 	
@@ -31,4 +59,66 @@ public class SingleEvent
 		return id;
 	}
 
+	public void SetYear(int newYear) throws Exception
+	{
+		try
+		{
+			if(newYear < 0)
+			{
+				throw new Exception();
+			}
+			year = newYear;
+		}
+		catch (Exception ex)
+		{
+			System.out.println("Year is " + newYear + ", year should be positive!");
+		}
+	}
+	
+	public int GetYear()
+	{
+		return year;
+	}
+
+	public void SetMonth(int newMonth) throws Exception
+	{
+		try
+		{
+			if((newMonth < 0) || (newMonth > 12))
+			{
+				throw new Exception();
+			}
+			month = newMonth;
+		}
+		catch (Exception ex)
+		{
+			System.out.println("Month is " + newMonth + ", month should be over 1 and below 12!");
+		}
+	}
+	
+	public int GetMonth()
+	{
+		return month;
+	}
+
+	public void SetDay(int newDay) throws Exception
+	{
+		try
+		{
+			if((newDay < 0) || (newDay > getMonthLastDay()))
+			{
+				throw new Exception();
+			}
+			day = newDay;
+		}
+		catch (Exception ex)
+		{
+			System.out.println("Day is " + newDay + ", WRONG!!");
+		}
+	}
+	
+	public int GetDay()
+	{
+		return day;
+	}
 }
